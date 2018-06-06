870209f68790ac08540f9d1956f568ab9936739c
Dhruv <devulapallidhruv@gmail.com>
2018-06-06 01:04:00 -0700

    Added TODO

--- p1.5/ortho.cc
+++ p1.5/ortho.cc
@@ -13,6 +13,7 @@ int main() {
     mps[0] = ITensor(Index("physical1", 2, Site), prevRight);
     randomize(mps[0]);
     for (int i = 2; i < N - 1; i++) {
+        //TO DO: What should the bond dimension be?
         Index left = Index("virtual2", 2, Link);
         Index physical = Index("physical", 2, Site);
         mps[i] = ITensor(prevRight, physical, left);
@@ -29,6 +30,8 @@ int main() {
         svd(mps[i], U, D, V, {"Cutoff", 0});
         mps[i] = V;
         mps[i - 1] = mps[N-2]*U*D;
+    }
+    //TO DO: Implement TEBD
 }
 
 
