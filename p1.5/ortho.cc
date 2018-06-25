2c48a4c66a20382c2cf43e776b75d09b0af858d0
Dhruv Devulapalli <devulapallidhruv@gmail.com>
2018-06-24 21:19:42 -0700

    Debugging svd issues

--- p1.5/ortho.cc
+++ p1.5/ortho.cc
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <iomanip>
 using namespace itensor;
+//Only the orthogonality center needs to be normalized
+//Whichever term gets the D
 
 ITensor makeOtherIndicesIdentity1(std::vector<ITensor> I, int j) {
     ITensor ret = ITensor(1.0);
@@ -16,7 +18,7 @@ ITensor makeOtherIndicesIdentity1(std::vector<ITensor> I, int j) {
 
 ITensor makeOtherIndicesIdentity2(std::vector<ITensor> I, int j) {
     ITensor ret = ITensor(1.0);
-    for(int i =0; i < I.size(); i++) {
+    for(int i = 0; i < I.size(); i++) {
         if (i!=j && i!=j+1) {
             ret = ret*I[i];
         }
@@ -27,16 +29,14 @@ ITensor makeOtherIndicesIdentity2(std::vector<ITensor> I, int j) {
 int main() {
     int N = 10;
     int steps = 10;
-    maxm = 15;
-    cutoff = 0;
+    int maxm = 15;
+    int cutoff = 0;
     //Coupling constants
     float J = 1.0;
     float h = 1.0;
     Real T = 0.001;
     SiteSet sites = SpinHalf(N);
-    OptSet opts;
-    opts.add("Cutoff",cutoff);
-    opts.add("Maxm",maxm);
+
 
     //DMRG
     auto hMPO = AutoMPO(sites);
@@ -51,7 +51,7 @@ int main() {
     hMPO += -J*0.5, "S-", N;
     //HMPO is to calculate the energy conveniently
     auto HMPO = MPO(hMPO);
-    auto dmrg_psi = MPS(sites)
+    auto dmrg_psi = MPS(sites);
     auto sweeps = Sweeps(5);
     sweeps.maxm() = 10,40,100,200,200;
     sweeps.cutoff() = 1E-8;
@@ -63,29 +63,30 @@ int main() {
     printfln("Ground state energy by dmrg = %.20f",dmrg_energy);
     //Create ITensor
     std::vector<ITensor> mps(N);
-    Index prevRight = Index("virtual1", Link);
+    std::vector<Index> virtualIndices(N - 1);
+    virtualIndices[0] = Index("virtual1", maxm, Link);
     mps[0] = ITensor(sites(1), prevRight);
     randomize(mps[0]);
     for (int i = 1; i < N - 1; i++) {
         //Max bond dimension of 15
-        Index left = Index("virtual2", 15, Link);
+        virtualIndices[i] = Index("virtual2", maxm, Link);
         //Index physical = Index("physical", 2, Site);
-        mps[i] = ITensor(prevRight, sites(i + 1), left);
-        prevRight = left;
+        mps[i] = ITensor(virtualIndices[i - 1], sites(i + 1), virtualIndices[i]);
         randomize(mps[i]);
     }
-    mps[N - 1] = ITensor(prevRight, Index("physical", 2, Site));
+    mps[N - 1] = ITensor(virtualIndices[N - 2], Index("physical", 2, Site));
     randomize(mps[N-1]);
-
+    printfln("Orthogonalizing:");
     //Canonical Form - initialize so that orthogonality center is site 1
     for (int i = N - 1; i > 1; i--) {
         ITensor V = mps[i]; //So V gets same indices
         ITensor D, U;
-        svd(mps[i], U, D, V, opts);
+        svd(mps[i], U, D, V, {"Cutoff", cutoff, "Maxm", maxm});
         mps[i] = V;
         mps[i - 1] = mps[N-2]*U*D;
+        mps[i - 1] /= norm(mps[i - 1]);
     }
-    //TO DO: Implement TEBD
+    printfln("TEBD:");
 
     for (int st = 0; st < steps; st++) {
         // Odd-even pairs (odd site first index)
@@ -96,23 +97,24 @@ int main() {
         	auto expTemp = expHermitian(hEven, -T);
         	//the mps should already have its orthocenter at i
         	auto p1 = mps[i];
-        	auto p2 =mps[i + 1];
+        	auto p2 = mps[i + 1];
         	auto p = p1*p2;
         	p = expTemp*p;
         	//p /= norm(p);
         	p = p.noprime();
         	auto U = mps[i];
         	ITensor D, V;
-        	svd(p, U, D, V, opts);
+        	svd(p, U, D, V, {"Cutoff", cutoff, "Maxm", maxm});
             mps[i] = U;
             mps[i + 1] = D*V;
-            //TO DO: How to keep it normalized?
+            mps[i+1] /= norm(mps[i+1]);
             //Set the orthocenter to be the next i value
             if (i < N - 2) {
                 U = mps[i + 1];
-                svd(mps[i+1], U, D, V, opts);
+                svd(mps[i+1], U, D, V, {"Cutoff", cutoff, "Maxm", maxm});
                 mps[i + 1] = U;
                 mps[i + 2] = D*V*mps[i + 2];
+                mps[i + 2] /= norm(mps[i + 1]);
             }
         }
         //If N is odd, then the orthocenter is N-1, and if N is even, it is N-2 (both of these cases contain N - 1)
@@ -121,7 +123,7 @@ int main() {
 
         //Even - odd pairs
         int start = (N % 2 == 0) ? N-2 : N-1;
-        for (int j = start; i > 1; i-=2) {
+        for (int i = start; i > 1; i-=2) {
         	ITensor hOdd = -h*ITensor(sites.op("Sz", i))*ITensor(sites.op("Sz", i + 1))
         	- J*0.5*ITensor(sites.op("S+", i))*ITensor(sites.op("Id", i + 1))
         	- J*0.5*ITensor(sites.op("S-", i))*ITensor(sites.op("Id", i + 1));
@@ -135,19 +137,23 @@ int main() {
         	p = p.noprime();
         	auto V = mps[i];
         	ITensor D, U;
-        	svd(p, U, D, V, opts);
+        	svd(p, U, D, V, {"Cutoff", cutoff, "Maxm", maxm});
             mps[i] = V;
             mps[i - 1] = U*D;
+            mps[i - 1] /= norm(mps[i - 1]);
             //TO DO: How to keep it normalized?
             //Set the orthocenter to be the next i value
             V = mps[i - 1];
-            svd(mps[i - 1], U, D, V, opts);
+            svd(mps[i - 1], U, D, V, {"Cutoff", cutoff, "Maxm", maxm});
             mps[i - 2] = mps[i - 1]*U*D;
+            mps[i - 2] /= norm(mps[i - 2]);
             mps[i - 1] = V;
         }
         //orthogonality center is now the first index (index0, site1)
     }
 
+//Check normalized
+printfln("\n Normalize check=%.10f", norm(mps[0]));
 //Calculate energy
 ITensor psi = ITensor(1.0);
 for (int i = 0; i < N; i++) {
@@ -163,7 +169,7 @@ for (int i = 0; i < N; i++) {
     - J*0.5*ITensor(sites.op("S+", i + 1))*makeOtherIndicesIdentity1(Id, i)
     - J*0.5*ITensor(sites.op("S-", i + 1))*makeOtherIndicesIdentity1(Id, i);
 }
-H += -J*0.5*ITensor(sites.op("S+", N))*makeOtherIndicesIdentity1(N - 1) - J*0.5*ITensor(sites.op("S-", N))*makeOtherIndicesIdentity1(N - 1);
+H += -J*0.5*ITensor(sites.op("S+", N))*makeOtherIndicesIdentity1(Id, N - 1) - J*0.5*ITensor(sites.op("S-", N))*makeOtherIndicesIdentity1(Id, N - 1);
 
 
 Real energy = (dag(prime(psi))*H*psi).real();
