3cf52a4ebdb8f593e6179d96246828eaeb30eff1
Dhruv Devulapalli <devulapallidhruv@gmail.com>
2018-07-19 01:39:06 -0700

    p1.5 working

--- p1.5/ortho.cc
+++ p1.5/ortho.cc
@@ -47,87 +47,27 @@ void normalizeCheck(std::vector<ITensor> mps, int N) {
 
 Real getEnergy(std::vector<ITensor> mps) {
   //TODO(Dhruv): Write this method to efficiently calculate the energy.
-  std::vector<ITensor> phi = mps;
-  for (int i = 0; i < N - 1; i+=2) {
-      ITensor hEven = -h*ITensor(sites.op("Sz", i + 1))*ITensor(sites.op("Sz", i + 2))
+  ITensor H, U, D, V;
+  Real energy = 0.0;
+  for (int i = 0; i < N - 1; i++) {
+      H = -h*ITensor(sites.op("Sz", i + 1))*ITensor(sites.op("Sz", i + 2))
       - J*0.5*ITensor(sites.op("S+", i + 1))*ITensor(sites.op("Id", i + 2))
       - J*0.5*ITensor(sites.op("S-", i + 1))*ITensor(sites.op("Id", i + 2));
-      //the mps should already have its orthocenter at i
-      auto p1 = mps[i];
-      auto p2 = mps[i + 1];
-      auto p = p1*p2;
-
-      p = hEven*p;
-      p = p.noprime();
-      ITensor U;
-      if (i > 0) {
-          U = ITensor(sites(i + 1),  commonIndex(mps[i], mps[i-1]));
+      ITensor psi = prime(prime(mps[i]*mps[i+1], sites(i+1)), sites(i+2));
+      Real e = (dag(psi)*H*mps[i]*mps[i+1]).real();
+      energy += e;
+      if (i == 0) {
+          U = ITensor(sites(i + 1));
       } else {
-          U = ITensor(sites(i + 1));//virtualIndices[i]);
+          U = ITensor(sites(i + 1), commonIndex(mps[i], mps[i-1]));
       }
-      ITensor D, V;
-      svd(p, U, D, V, {"Cutoff", cutoff, "Maxm", maxm});
+      svd(mps[i]*mps[i+1], U, D, V);
       mps[i] = U;
-      mps[i + 1] = D*V;
-      //mps[i+1] /= norm(mps[i+1]);
-      //Set the orthocenter to be the next i value
-      if (i < N - 2) {
-          ITensor D, V;
-          U = ITensor(sites(i + 2), commonIndex(mps[i+1], mps[i]));//virtualIndices[i + 1]);
-          svd(mps[i+1], U, D, V, {"Cutoff", cutoff, "Maxm", maxm});
-          mps[i + 1] = U;
-          // mps[i + 1] /= norm(mps[i + 1]);
-          mps[i + 2] = D*V*mps[i + 2];
-          //mps[i + 2] /= norm(mps[i + 2]);
-      }
+      mps[i+1] = D*V;
   }
-  //If N is odd, then the orthocenter is N-1, and if N is even, it is N-2 (both of these cases contain N - 1)
-  ITensor hlast = -J*0.5*ITensor(sites.op("S+", N)) - J*0.5*ITensor(sites.op("S-", N));
-  mps[N - 1] = (mps[N-1]*hlast).noprime();
-  //mps[N - 1] /= norm(mps[N -1]);
-
-  // Make sure that when N is even that the ortho center is properly pushed over from N-1 (where it ends up) to N-2.
-  if (N % 2 == 0) {
-      ITensor U = ITensor(commonIndex(mps[N-1], mps[N-2]));
-      ITensor D, V;
-      svd(mps[N - 1], U, D, V, {"Cutoff", cutoff, "Maxm", maxm});
-      mps[N - 1] = V;
-      mps[N - 2] = mps[N - 2]*U*D;
-      //mps[N - 2] /= norm(mps[N - 2]);
-  }
-  //Even - odd pairs
-  int start = (N % 2 == 0) ? N-2 : N-1;
-  for (int i = start; i > 1; i-=2) {
-      ITensor hOdd = -h*ITensor(sites.op("Sz", i))*ITensor(sites.op("Sz", i + 1))
-      - J*0.5*ITensor(sites.op("S+", i))*ITensor(sites.op("Id", i + 1))
-      - J*0.5*ITensor(sites.op("S-", i))*ITensor(sites.op("Id", i + 1));
-      auto p1 = mps[i - 1];
-      auto p2 =mps[i];
-      auto p = p1*p2;
-      p = hOdd*p;
-      //p /= norm(p);
-      p = p.noprime();
-      ITensor D, V, U(sites(i), commonIndex(mps[i-1], mps[i-2]));//, commonIndex(mps[i], mps[i-1]));//virtualIndices[i - 1]);
-      svd(p, U, D, V, {"Cutoff", cutoff, "Maxm", maxm});
-      mps[i] = V;
-      mps[i - 1] = U*D;
-      //mps[i - 1] /= norm(mps[i - 1]);
-      U = ITensor(commonIndex(mps[i-1], mps[i - 2]));
-      svd(mps[i - 1], U, D, V, {"Cutoff", cutoff, "Maxm", maxm});
-      mps[i - 2] = mps[i - 2]*U*D;
-      //mps[i - 2] /= norm(mps[i - 2]);
-      mps[i - 1] = V;
-  }
-  normalizeCheck(phi, N);
-  normalizeCheck(mps, N);
-  ITensor psi = ITensor(1.0);
-  ITensor psi2 = ITensor(1.0);
-  for (int i = 0; i < N; i++) {
-      //Print(mps[i]);
-      psi = psi*mps[i];
-      psi2 = psi2*phi[i];
-  }
-  return (dag(psi)*psi2).real();
+  H = -J*0.5*ITensor(sites.op("S+", N)) - J*0.5*ITensor(sites.op("S-", N));
+  energy += (dag(prime(mps[N-1], sites(N)))*H*mps[N-1]).real();
+  return energy;
 }
 
 int main() {
@@ -195,12 +135,15 @@ int main() {
         mps[i - 1] = mps[i - 1]*U*D;
         mps[i - 1] /= norm(mps[i - 1]);
     }
-    for (int j = 0; j < N; j++) {
-        printfln("j=%d, rank=%d", j, rank(mps[j]));
-    }
     printfln("TEBD:");
+    float progress = 0.0;
+    int barWidth = 70;
+
 
     for (int st = 0; st < steps; st++) {
+        if (st % 100 == 0) {
+            Print(st);
+        }
         // Odd-even pairs (odd site first index)
         for (int i = 0; i < N - 1; i+=2) {
         	ITensor hEven = -h*ITensor(sites.op("Sz", i + 1))*ITensor(sites.op("Sz", i + 2))
@@ -290,7 +233,7 @@ int main() {
 normalizeCheck(mps, N);
 //Calculate energy
 
-ITensor psi = ITensor(1.0);
+/*ITensor psi = ITensor(1.0);
 for (int i = 0; i < N; i++) {
     //Print(mps[i]);
     psi = psi*mps[i];
@@ -306,9 +249,10 @@ printfln("Constructing H");for (int i = 0; i < N-1; i++) {
     - J*0.5*ITensor(sites.op("S-", i + 1))*makeOtherIndicesIdentity1(Id, i);
 }
 H += -J*0.5*ITensor(sites.op("S+", N))*makeOtherIndicesIdentity1(Id, N - 1) - J*0.5*ITensor(sites.op("S-", N))*makeOtherIndicesIdentity1(Id, N - 1);
+*/
 printfln("Calculating energy");
-printfln("\nGround state energy by TEBD= %.10f", getEnergy(mps));
-Real energy = (dag(prime(psi))*H*psi).real();
-printfln("\nGround state energy by TEBD= %.20f",energy);
-printfln("Ground state energy by dmrg = %.20f",dmrg_energy);
+printfln("\nGround state energy by TEBD = %.25f", getEnergy(mps));
+/*Real energy = (dag(prime(psi))*H*psi).real();
+printfln("\nGround state energy by TEBD= %.20f",energy);*/
+printfln("Ground state energy by dmrg = %.25f",dmrg_energy);
 }
